d421bee9f541084b72cc5af0e44c9d7151e88cf2
Arthur <arthurjenckdev@gmail.com>
2026-02-25 10:10:05 +0100

    feat/player: join screen

--- player-app/src/components/JoinScreen.tsx
+++ player-app/src/components/JoinScreen.tsx
@@ -16,33 +16,57 @@ interface JoinScreenProps {
  * Composant formulaire pour rejoindre un quiz existant.
  *
  * Ce qu'il faut implementer :
- * - Un champ pour le code du quiz (6 caracteres, majuscules)
- *   avec la classe .code-input pour le style monospace
- * - Un champ pour le pseudo du joueur
- * - Un bouton "Rejoindre" (classe .btn-primary)
- * - Afficher le message d'erreur s'il existe (classe .error-message)
- * - Valider que les deux champs ne sont pas vides avant d'appeler onJoin
+ * - Un champ pour le code du quiz (6 caracteres, majuscules) V
+ *   avec la classe .code-input pour le style monospace V
+ * - Un champ pour le pseudo du joueur V
+ * - Un bouton "Rejoindre" (classe .btn-primary) V
+ * - Afficher le message d'erreur s'il existe (classe .error-message) V
+ * - Valider que les deux champs ne sont pas vides avant d'appeler onJoin V
  *
  * Classes CSS disponibles : .join-form, .form-group, .code-input,
  * .error-message, .btn-primary
  */
 function JoinScreen({ onJoin, error }: JoinScreenProps) {
-  const [quizCode, setQuizCode] = useState<number | undefined>(undefined)
+  const [quizCode, setQuizCode] = useState<string | undefined>(undefined)
   const [nick, setNick] = useState<string | undefined>(undefined)
+  const [errorToDisplay, setErrorToDisplay] = useState<string | undefined>(
+    error,
+  )
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    // TODO: Valider que les champs ne sont pas vides
-    // TODO: Appeler onJoin(code.toUpperCase(), name)
+
+    if (!quizCode || !nick) {
+      setErrorToDisplay('Veuillez remplir tous les champs.')
+      return
+    }
+
+    onJoin(quizCode, nick)
   }
 
   return (
-    <form className="join-form" onSubmit={handleSubmit}>
+    <form className="join-form form-group" onSubmit={handleSubmit}>
       <h1>Rejoindre un Quiz</h1>
-      {/* TODO: Afficher l'erreur si elle existe */}
-      {/* TODO: Champ code du quiz avec classe .code-input */}
-      {/* TODO: Champ pseudo */}
-      {/* TODO: Bouton Rejoindre */}
+      {errorToDisplay && <p className="error-message">{errorToDisplay}</p>}
+      <input
+        className="code-input"
+        type="text"
+        name="quizCode"
+        id="quizCode"
+        minLength={6}
+        maxLength={6}
+        onChange={(e) => setQuizCode(e.target.value.toLocaleUpperCase())}
+      />
+      <input
+        type="text"
+        name="nick"
+        id="nick"
+        maxLength={16}
+        onChange={(e) => setNick(e.target.value)}
+      />
+      <button type="submit" className="btn-primary">
+        Rejoindre
+      </button>
     </form>
   )
 }
